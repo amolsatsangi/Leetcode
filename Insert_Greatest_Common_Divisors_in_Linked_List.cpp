05c61f82d1af62ca9416417812193a04624c6081
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2024-09-10 22:05:30 +0530

    Create Insert_Greatest_Common_Divisors_in_Linked_List.cpp

---
+++ Insert_Greatest_Common_Divisors_in_Linked_List.cpp
@@ -0,0 +1,81 @@
+/*************************************************
+Medium
+
+Given the head of a linked list head, in which each node contains an integer value.
+
+Between every pair of adjacent nodes, insert a new node with a value equal to the greatest common divisor of them.
+
+Return the linked list after insertion.
+
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+Example 1:
+
+Input: head = [18,6,10,3]
+Output: [18,6,6,2,10,1,3]
+Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes (nodes in blue are the inserted nodes).
+- We insert the greatest common divisor of 18 and 6 = 6 between the 1st and the 2nd nodes.
+- We insert the greatest common divisor of 6 and 10 = 2 between the 2nd and the 3rd nodes.
+- We insert the greatest common divisor of 10 and 3 = 1 between the 3rd and the 4th nodes.
+There are no more adjacent nodes, so we return the linked list.
+
+Example 2:
+Input: head = [7]
+Output: [7]
+Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes.
+There are no pairs of adjacent nodes, so we return the initial linked list.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5000].
+1 <= Node.val <= 1000
+
+
+                                                                  ########################################
+                                                                  # Author - @Amol Satsangi              #
+                                                                  # Date - 10 Sept 2024                  #
+                                                                  # Used equilateral thorem of GCD       #
+                                                                  # GCD(a,b) = GCD (a-b,b) = GCD (a%b,b) #
+                                                                  ########################################
+
+                                                                  
+**************************************/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        if(head ->next == nullptr || head == nullptr)
+            return head;
+        else{
+            ListNode *p = head, *q = head ->next, *node;
+            while(q!=nullptr){
+                int a = p->val, b = q->val;
+                //using gcd (a,b) = (a%b, b) 
+                while(a!=0 && b!= 0){
+                if(a>b) a = a%b;
+                else b = b%a;
+                }
+                int gcd;
+                if(a==0) gcd =b;
+                else gcd = a;
+                node =  new ListNode(gcd);
+                p->next = node;
+                node -> next = q;
+                p =  q;
+                q = q->next;
+            }
+        }
+        return head;
+    }
+};

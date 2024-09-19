e37b7741e7e7c7ea6b213486ec88eeffccffbe24
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2024-09-19 23:07:10 +0530

    Create  Middle_of_the_Linked_List.cpp

---
+++ Easy/Middle of the Linked List/Middle_of_the_Linked_List.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode * p = head, *q = head;
+        while(q!=nullptr && q->next!= nullptr){
+            p = p->next;
+            q = q->next ->next;
+        }
+        return p;
+    }
+};

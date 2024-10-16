89bb483009dd1565999afb714c26140495a156f8
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2024-10-16 22:36:33 +0530

    Create Find Words Containing Character.cpp

---
+++ Easy/Find Words Containing Character/Find Words Containing Character.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> index;
+        for(int i =0;i<words.size();i++)
+        {
+            for(auto c:words[i]){
+                if(c==x){
+                    index.push_back(i);
+                    break;
+                }
+            }
+        }
+    return index;
+    }
+};

8df463be49acc4eddedf1bcfbe547f720c0bf085
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2024-11-02 23:01:20 +0530

    Create  Circular Sentence.cpp

---
+++ Easy/Circular Sentence/Circular Sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        if(sentence[0] != sentence[sentence.size()-1])
+            return false;
+        else{
+            for(int i=0;i<sentence.size();i++)
+            {
+                if(sentence[i] == ' ')
+                    if(sentence[i-1] != sentence[i+1])
+                        return false;
+            }
+        }
+    return true;
+    }
+};

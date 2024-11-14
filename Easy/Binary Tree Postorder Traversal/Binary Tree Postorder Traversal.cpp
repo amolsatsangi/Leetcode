5b4faec7f5c52ef9549a319014efd211abf3723c
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2024-11-14 19:12:49 +0530

    Create Binary Tree Postorder Traversal.cpp

---
+++ Easy/Binary Tree Postorder Traversal/Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorder{};
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root!=nullptr){
+            postorderTraversal(root->left);
+            postorderTraversal(root->right);
+            postorder.push_back(root->val);
+        }
+    return postorder;
+    }
+};

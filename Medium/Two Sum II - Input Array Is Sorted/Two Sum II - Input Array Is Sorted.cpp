39e41fb10edde58cf74da8667f740e7d155583f1
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2024-11-22 23:26:40 +0530

    Create Two Sum II - Input Array Is Sorted.cpp

---
+++ Medium/Two Sum II - Input Array Is Sorted/Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i{0}, j=numbers.size()-1;
+        vector<int>ans;
+        while(i!=j){
+            if(numbers[i]+numbers[j] == target){
+                ans.push_back(i+1);
+                ans.push_back(j+1);
+                break;
+            }
+            if(target>numbers[i]+numbers[j])
+                i++;
+            else
+                j--;                
+        }
+        return ans;
+    }
+};

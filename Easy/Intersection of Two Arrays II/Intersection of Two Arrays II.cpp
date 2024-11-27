bb73a0fcae9e5f10b247ac6a229288bd0d2a24c2
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2024-11-27 10:18:25 +0530

    Create Intersection of Two Arrays II.cpp

---
+++ Easy/Intersection of Two Arrays II/Intersection of Two Arrays II.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> intersection_map{};
+        vector<int> intersection_vector{};
+        for(int i=0;i<nums1.size();i++){
+            intersection_map[nums1[i]] =  intersection_map[nums1[i]]+1;
+        }
+        sort(nums2.begin(), nums2.end());
+        int last = -1;
+        for(int j=0;j<nums2.size();j++){
+            if(intersection_map.find(nums2[j])!=intersection_map.end()){
+               if(last == nums2[j] && intersection_map[nums2[j]] == 0){
+                    last = nums2[j];
+                }
+                else{
+                    intersection_vector.push_back(nums2[j]);
+                    last = nums2[j];
+                    intersection_map[nums2[j]]--;
+                }
+            }
+        }
+    return intersection_vector;
+    }
+};

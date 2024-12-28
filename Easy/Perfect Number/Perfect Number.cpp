fcf429fea5a3a5f1c358b9d6035269d192536948
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2024-12-28 11:04:47 +0530

    Create Perfect Number.cpp

---
+++ Easy/Perfect Number/Perfect Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum{1};
+        if(num==1)
+            return false;
+        for(int i=2;i<=sqrt(num);i++){
+            if(num%i==0){
+                sum+=i;
+                sum+=(num/i);
+            }
+        }
+    return sum==num;
+    }
+};

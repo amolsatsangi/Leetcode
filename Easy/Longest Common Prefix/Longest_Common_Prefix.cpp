694602ff8071d68b48d3591d912d0fef1eb4b44f
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-01-04 18:29:42 +0530

    Update Longest_Common_Prefix.cpp

--- Easy/Longest Common Prefix/Longest_Common_Prefix.cpp
+++ Easy/Longest Common Prefix/Longest_Common_Prefix.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    string find_common_prefix_between_two_strings(string str1, string str2){
-        string prefix ="";
-        int i{0}, j{0};
-        while(i<str1.size() && j< str2.size() && str1[i]==str2[j]){
-            prefix+=str1[i];
-            i++;j++;
-        }
-        return prefix;
-    }
     string longestCommonPrefix(vector<string>& strs) {
-        string pre =strs[0];
-       for(int i=1;i<strs.size();i++)
-       {
-            pre = find_common_prefix_between_two_strings(pre, strs[i]);
-            if(pre=="")
-                return "";
-       }
-    return pre;
+        string ans_str{};
+        int i{0}, n = strs.size();
+        sort(strs.begin(),strs.end());
+        while(i<strs[0].size() && i<strs[n-1].size()){
+            if(strs[0][i]!=strs[n-1][i])
+                return ans_str;
+            ans_str+=strs[0][i];
+            i++;
+        }
+    return ans_str;
     }
 };

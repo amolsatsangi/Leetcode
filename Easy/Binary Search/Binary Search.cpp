7eb72d6deed564d412ccef528d3cfb7c707dc142
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-01-12 12:06:12 +0530

    Create Binary Search.cpp

---
+++ Easy/Binary Search/Binary Search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start{0},end=nums.size()-1,mid=(start+end)/2;
+        while(start<=end){
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                end=mid-1;
+            else
+                start=mid+1;
+            mid=(start+end)/2;
+        }
+    return -1;
+    }
+};

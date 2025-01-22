c6bf6ad64242a0413f63f1b66e31928be2045142
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-01-22 18:21:35 +0530

    Update Pascal's Triangle.cpp

--- Easy/Pascal's Triangle/Pascal's Triangle.cpp
+++ Easy/Pascal's Triangle/Pascal's Triangle.cpp
@@ -3,10 +3,7 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> pascal;
         pascal.push_back({1});
-        if(numRows==1)
-            return pascal;
-        pascal.push_back({1,1});
-        for(int i=1;i<numRows-1;i++){
+        for(int i=0;i<numRows-1;i++){
             vector<int> temp;
             for(int j=0;j<pascal[i].size();j++){
                 if(j==0)

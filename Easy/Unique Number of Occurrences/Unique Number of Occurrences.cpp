13cecd281721cdae53e8b8442b6c63a921968629
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-02-09 10:39:14 +0530

    Update Unique Number of Occurrences.cpp

--- Easy/Unique Number of Occurrences/Unique Number of Occurrences.cpp
+++ Easy/Unique Number of Occurrences/Unique Number of Occurrences.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        vector<int> v;
-
-        for (int i = 0; i < arr.size(); i++) {
-            int cnt = 1;
-
-            // Count occurrences of the current element
-            while (i + 1 < arr.size() && arr[i] == arr[i + 1]) {
-                cnt++;
+        vector<int> freq;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            int count{1};
+            while(i+1<arr.size() && arr[i]==arr[i+1]){
+                count++;
                 i++;
             }
-
-            v.push_back(cnt);
+            freq.push_back(count);
         }
-
-        sort(v.begin(), v.end());
-
-        for (int i = 1; i < v.size(); i++) {
-            if (v[i] == v[i - 1]) {
+        sort(freq.begin(),freq.end());
+        for(int i=0;i<freq.size()-1;i++){
+            if(freq[i]==freq[i+1])
                 return false;
-            }
         }
-
-        return true;
+    return true;
     }
-}; 
+};

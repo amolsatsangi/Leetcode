8a21f872a7ef8982ed528a9ddfac84c85ee87558
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-02-18 19:29:06 +0530

    Create Average Salary Excluding the Minimum and Maximum Salary.cpp

---
+++ Easy/Average Salary Excluding the Minimum and Maximum Salary/Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        int maxi{INT_MIN}, mini{INT_MAX};
+        double sum{0};
+        for(int i=0;i<salary.size();i++){
+            sum +=salary[i];
+            maxi=max(maxi,salary[i]);
+            mini=min(mini,salary[i]);
+        }
+        sum -=(maxi+mini);
+        return sum/(salary.size()-2);
+    }
+};

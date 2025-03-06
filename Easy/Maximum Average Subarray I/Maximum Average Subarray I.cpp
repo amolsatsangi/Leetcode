87155048f22982cc555a6f2b4602c1f25b49c67b
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-03-06 19:38:47 +0530

    Update Maximum Average Subarray I.cpp

--- Easy/Maximum Average Subarray I/Maximum Average Subarray I.cpp
+++ Easy/Maximum Average Subarray I/Maximum Average Subarray I.cpp
@@ -3,14 +3,12 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
         for(int i=1;i<nums.size();i++)
             nums[i]=nums[i]+nums[i-1];
-        vector<int> arr;
-        for(int i=0;i<k;i++)
-            arr.push_back(nums[i]);
-        for(int i=k;i<nums.size();i++)
-            arr.push_back(nums[i]-nums[i-k]);
-        int maxi{INT_MIN};
-        for(int i=k-1;i<arr.size();i++)
-            maxi=max(maxi,arr[i]);
+        int maxi{nums[k-1]};
+        for(int i=k;i<nums.size();i++){
+            cout<<(nums[i]-nums[i-k])<<endl;
+            maxi = max(maxi,(nums[i]-nums[i-k]));
+        }
+        if(nums.size()==k)
+            return double(nums[k-1])/k;
         return double(maxi)/k;
     }
-};

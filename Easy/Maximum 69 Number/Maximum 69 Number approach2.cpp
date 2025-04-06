f89ee86e84a1dd5a01e0690df7cb6ccecddd9a45
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-04-06 14:42:46 +0530

    Create Maximum 69 Number approach2.cpp

---
+++ Easy/Maximum 69 Number/Maximum 69 Number approach2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        int ans;
+        string num_str = to_string(num);
+        for(int i=0;i<num_str.size();i++){
+            if(num_str[i]=='6'){
+                num_str[i]='9';
+                break;
+            }
+        }
+        return stoi(num_str);
+    }
+};

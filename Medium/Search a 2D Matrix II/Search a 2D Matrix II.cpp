cc34cb27b437e7e29f1e1ab43b3ae29fc6f784b0
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-04-21 23:28:44 +0530

    Create Search a 2D Matrix II.cpp

---
+++ Medium/Search a 2D Matrix II/Search a 2D Matrix II.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int top{0}, bottom= matrix.size()-1;
+        int mid{0};
+        while(top<=bottom){
+            mid = (top+bottom)/2;
+            if(matrix[mid][0]==target)
+                return true;
+            else if(matrix[mid][0]>target){
+                bottom = mid-1;
+            }
+            else
+                top = mid+1;
+        }
+        if(bottom == -1)
+            return false;
+        for(int i=0;i<=bottom;i++){
+            int left{0}, right= matrix[0].size()-1;
+            while(left<=right){
+                mid = (left+right)/2;
+                if(matrix[i][mid]==target)
+                    return true;
+                else if(matrix[i][mid]>target){
+                    right = mid-1;
+                }
+                else
+                    left = mid+1;
+            }
+        }
+    return false;
+    }
+};

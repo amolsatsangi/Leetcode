15918b7252f2a4817182af96bd9cf1f957651512
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-05-15 22:38:51 +0530

    Create Longest Unequal Adjacent Groups Subsequence I.cpp

---
+++ Easy/Longest Unequal Adjacent Groups Subsequence I/Longest Unequal Adjacent Groups Subsequence I.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        vector<string> strs;
+        strs.push_back(words[0]);
+        for(int i=1;i<words.size();i++){
+            if(groups[i]!=groups[i-1])
+                strs.push_back(words[i]);
+        }
+    return strs;
+    }
+};

70a6aa0a2dac849b1cc1310595edcaf022e34ed3
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-06-05 23:34:51 +0530

    Update Final Array State After K Multiplication Operations I.cpp

--- Easy/Final Array State After K Multiplication Operations I/Final Array State After K Multiplication Operations I.cpp
+++ Easy/Final Array State After K Multiplication Operations I/Final Array State After K Multiplication Operations I.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         for(int i=0;i<k;i++){
             int mini{INT_MAX};
-            vector<int> index;
             for(int j=0;j<nums.size();j++){
                     mini = min(mini,nums[j]);
             }

f2d3e50ba599d4351398a2aa2c507ffb7b1dcbc3
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-06-22 22:25:51 +0530

    Create Sum of Squares of Special Elements.cpp

---
+++ Easy/Sum of Squares of Special Elements/Sum of Squares of Special Elements.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int ans{0};
+        for(int i=0;i<nums.size();i++){
+            if(nums.size()%(i+1)==0)
+                ans+=(nums[i]*nums[i]);
+        }
+    return ans;
+    }
+};

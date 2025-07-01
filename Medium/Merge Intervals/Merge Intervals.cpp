d7cf52988a7a7eafded8cb6a789fbe35987e3131
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-07-01 20:18:39 +0530

    Create Merge Intervals.cpp

---
+++ Medium/Merge Intervals/Merge Intervals.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        for(auto it: intervals){
+            if(ans.empty() || ans.back()[1] < it[0])
+                ans.push_back(it);
+            else
+                ans.back()[1] = max(ans.back()[1], it[1]);
+        }
+        return ans;
+    }
+};

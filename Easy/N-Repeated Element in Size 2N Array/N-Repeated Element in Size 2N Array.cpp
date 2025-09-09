fa7e9f61c895ffab32734e67093cd0b0581a432c
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-09-09 22:03:29 +0530

    Create N-Repeated Element in Size 2N Array.cpp

---
+++ Easy/N-Repeated Element in Size 2N Array/N-Repeated Element in Size 2N Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        int n=nums.size()/2;
+        unordered_map<int,int> umap;
+        for(int i=0;i<nums.size();i++){
+            umap[nums[i]]++;
+            if (umap[nums[i]]>1)
+                return nums[i];
+        }
+    return -1;
+    }
+};

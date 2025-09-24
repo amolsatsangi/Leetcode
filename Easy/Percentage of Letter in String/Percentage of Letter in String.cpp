2fc6a79957ade9a7cf6dc762c525386e54658102
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-09-24 22:58:13 +0530

    Create Percentage of Letter in String.cpp

---
+++ Easy/Percentage of Letter in String/Percentage of Letter in String.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int count{0};
+        for(char c:s){
+            if(c == letter)
+                count++;
+        }
+    return count ==0 ? 0: (count*100)/s.size();
+    }
+};

57960e9be7e1f104ed0b7d0e38f8e2d75ee96d44
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-11-21 18:09:20 +0530

    Create Minimum Number of Vertices to Reach All Nodes.cpp

---
+++ Medium/Minimum Number of Vertices to Reach All Nodes/Minimum Number of Vertices to Reach All Nodes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int> vis(n,0);
+        vector<int> ans;
+        for(int i=0;i<edges.size();i++){
+            vis[edges[i][1]] = 1;
+        }
+        for(int i=0;i<vis.size();i++){
+            if(vis[i]==0)
+                ans.push_back(i);
+        }
+    return ans;
+    }
+};

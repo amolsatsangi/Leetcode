7a908b881af88943ad4015b03934a899513278fa
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-11-24 00:14:15 +0530

    Update Cousins in Binary Tree.cpp

--- Easy/Cousins in Binary Tree/Cousins in Binary Tree.cpp
+++ Easy/Cousins in Binary Tree/Cousins in Binary Tree.cpp
@@ -11,14 +11,25 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode* root, int val, int & d){
-        preorder(root->left,val,d+1);
-        preorder(root->right,val,d+1);
+    void preorder(TreeNode* root, int val, int d, int & dis, TreeNode* &p, TreeNode * parent){
+        if(root == nullptr)
+            return;
+        if(root->val == val){
+            dis =d;
+            p = parent;
+        }
+        preorder(root->left,val,d+1,dis,p,root);
+        preorder(root->right,val,d+1,dis,p,root);
+       
     }
     bool isCousins(TreeNode* root, int x, int y) {
-        int d1 = preorder(root,x,0);
-        int d2 = preorder(root,y,0);
-        cout<<d1<<d2<<endl;
-        return d1 == d2;
+        int d1,d{0};
+        TreeNode * p1{nullptr}, * p2{nullptr},*p;
+        preorder(root,x,d,d1,p1,p);
+        int d2;
+        d=0;
+        p =nullptr;
+        preorder(root,y,d,d2,p2,p);
+        return (d1 == d2 && p1!=p2);
     }
 };

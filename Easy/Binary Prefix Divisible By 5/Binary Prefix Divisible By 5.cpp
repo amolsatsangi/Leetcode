06eca9d6340a66d33f344ca73f2859331e942181
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-11-24 23:51:07 +0530

    Create Binary Prefix Divisible By 5.cpp

---
+++ Easy/Binary Prefix Divisible By 5/Binary Prefix Divisible By 5.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> vec;
+        int val{0};
+        for(int i=0;i<nums.size();i++){
+            
+        }
+    }
+};

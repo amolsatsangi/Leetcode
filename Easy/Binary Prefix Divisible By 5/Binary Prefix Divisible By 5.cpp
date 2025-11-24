8da73d6876565f5d0f3d1957844f741dd3ded6df
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2025-11-25 00:55:18 +0530

    Update Binary Prefix Divisible By 5.cpp

--- Easy/Binary Prefix Divisible By 5/Binary Prefix Divisible By 5.cpp
+++ Easy/Binary Prefix Divisible By 5/Binary Prefix Divisible By 5.cpp
@@ -4,7 +4,12 @@ public:
         vector<bool> vec;
         int val{0};
         for(int i=0;i<nums.size();i++){
-            
+            val = ((val<<1)+nums[i])%5;
+            if(val==0)
+                vec.push_back(1);
+            else
+                vec.push_back(0);
         }
+        return vec;
     }
 };
